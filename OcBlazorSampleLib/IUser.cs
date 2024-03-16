33d99a2cba5d739632ceb6405ff00100c06b5c17
Sijmen Koffeman <koffemans@hotmail.com>
2024-03-16 18:14:37 +0000

    first scratches

---
+++ OcBlazorSampleLib/IUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcBlazorSampleLib;
+
+public interface IUser
+{
+    /// <summary>
+    /// Gets the user name.
+    /// </summary>
+    string UserName { get; }
+}
